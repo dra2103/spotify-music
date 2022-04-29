8deebe4ec066f528a4fddfc0067c61c31780b6fa
Daniel Adamski <dra2103@gmail.com>
2022-04-29 11:03:15 -0700

    Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="main-heading">Spotify Music Search</h1>
+        </div>
+    );
+};
+
+export default Header;
